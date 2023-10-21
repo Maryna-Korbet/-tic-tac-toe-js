f2cbf41c4f01f09e46a0373d7d8dc4a0006199c6
Maryna-Korbet <139797358+KorbetMaryna@users.noreply.github.com>
2023-10-22 01:29:02 +0300

    added field markings

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-import { combination } from "./combination";
+// import { combination } from "./combination";
 
+
+const content = document.querySelector('.content');
+
+let markup = '';
+for (let i = 0; i < 9; i += 1){
+    markup += `<div class="item"></div>`;
+}
+// console.log(markup);
+
+content.insertAdjacentHTML("afterbegin", markup);
